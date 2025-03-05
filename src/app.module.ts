b9a3f2ae34c56db03f6bdcaa7f01047f8364b39e
mtthgomes <nascimento.gomes@icloud.com>
2025-03-05 13:37:51 -0300

    lok

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,12 @@
 import { Global, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { JwtModule } from '@nestjs/jwt';
 import { DatabaseModule } from 'src/shared/database/database.module';
 import { MyLogger } from 'src/shared/services/logger.service';
-import { LOGSModule } from './modules/logs/logs.module';
 import { PrismaService } from './shared/database/prisma.service';
 import { UserGlobalModule } from './modules/user/user.module';
 import { DoctorGlobalModule } from './modules/doctor/doctor.module';
+import { LOGSModule } from './modules/logs/logs.module';
 
 @Global()
 @Module({

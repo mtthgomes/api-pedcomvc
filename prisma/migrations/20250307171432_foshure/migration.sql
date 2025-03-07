381c8951aeee788876961b4623cbbdc13f53b4fe
mtthgomes <nascimento.gomes@icloud.com>
2025-03-07 18:27:34 -0300

    deploy

--- prisma/migrations/20250307171432_foshure/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[getStreamChatId]` on the table `Chat` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX `Chat_getStreamChatId_key` ON `Chat`(`getStreamChatId`);
